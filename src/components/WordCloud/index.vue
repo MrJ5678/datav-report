7dd79faf287a5cb8d8c76763592513165ad0743e
hhhhhq <Mr.j@tom.com>
2021-05-04 00:18:22 +0800

    before add axios

--- src/components/WordCloud/index.vue
+++ src/components/WordCloud/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <ve-worldcloud :data="chartData"/>
+  <ve-wordcloud :data="chartData" height="100%" :settings="settings"/>
 </template>
 
 <script>
@@ -12,7 +12,37 @@ export default {
         rows: [{
           name: '建中网络',
           value: 100
+        }, {
+          name: '建中网络',
+          value: 10
+        }, {
+          name: '建中网络',
+          value: 20
+        }, {
+          name: '建中网络',
+          value: 30
+        }, {
+          name: '建中网络',
+          value: 40
+        }, {
+          name: '建中网络',
+          value: 50
+        }, {
+          name: '建中网络',
+          value: 60
+        }, {
+          name: '建中网络',
+          value: 70
+        }, {
+          name: '建中网络',
+          value: 80
+        }, {
+          name: '建中网络',
+          value: 90
         }]
+      },
+      settings: {
+        color: ['rgba(97, 216, 0, 0.7)', 'rgba(204, 178, 26, 0.7)', 'rgba(245, 166, 35, 0,7)', 'rgba(156, 13, 113, 0.7)']
       }
     }
   }
